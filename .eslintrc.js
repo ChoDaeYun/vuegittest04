185fcdde1d7e1198666b302d3e1a293d43a8d75b
조대연 [Cho <chody0116@neople.co.kr>
2019-04-30 17:47:08 +0900

    vue login test

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,14 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier":[
+      "warn",
+      {
+        "semi":false,
+        "trailingComma":"none"
+      }
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
